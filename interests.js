e718ee527b34e67720c26a1d6b51d77229d497df
richiezizm <102637770+richiezizm@users.noreply.github.com>
2023-07-27 16:36:24 -0400

    Add files via upload

---
+++ interests.js
@@ -0,0 +1,22 @@
+// small examples of different Japanese alphabets
+const hiragana1 = ["あ い う え お"];
+const hiragana2 = ["a i u e o"];
+
+const katakana1 = ["リチア-ド"];
+const katakana2 = ["Richard"];
+
+const kanji1 = ["愛"];
+const kanji2 = ["Love"];
+
+// function for writing out favorite games (top 5)
+// make sure to include: genre, platform, year released
+
+function favoriteGames (string, string2, number) {
+    console.log(string);
+    console.log(string2);
+    console.log(number);
+}
+
+const FF7 = favoriteGames("RPG", "PSX", 1997);
+console.log(FF7);
+// can comment out the above later, just testing if it prints
